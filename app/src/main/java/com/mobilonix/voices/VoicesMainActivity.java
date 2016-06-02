76238d6cedbbbfcd959efde452cd4c65a3371773
squoraishee <coruscatingluminostiy@gmail.com>
2016-06-02 17:16:04 -0400

    First commit of Voices Redesign

---
+++ app/src/main/java/com/mobilonix/voices/VoicesMainActivity.java
@@ -0,0 +1,13 @@
+package com.mobilonix.voices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoicesMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voices_main);
+    }
+}
