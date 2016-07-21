7c8144b641deb0e721d0536a2669dbb6a7ad611c
squoraishee <coruscatingluminostiy@gmail.com>
2016-07-21 09:32:42 -0400

    Add dumy layout for all groups

--- app/src/main/java/com/mobilonix/voices/VoicesMainActivity.java
+++ app/src/main/java/com/mobilonix/voices/VoicesMainActivity.java
@@ -12,6 +12,10 @@ import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Call;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.Toolbar;
 
@@ -55,8 +59,17 @@ public class VoicesMainActivity extends AppCompatActivity implements LocationLis
     private void initViews() {
         mainContentFrame = (FrameLayout)findViewById(R.id.main_content_frame);
         setSupportActionBar((android.support.v7.widget.Toolbar)findViewById(R.id.primary_toolbar));
+        findViewById(R.id.primary_toolbar).setVisibility(View.GONE);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.primary_menu, menu);
+
+        return true;
+    }
 
     public FrameLayout getMainContentFrame() {
         return mainContentFrame;
--- app/src/main/java/com/mobilonix/voices/representatives/ui/RepresentativesPageLayout.java
+++ app/src/main/java/com/mobilonix/voices/representatives/ui/RepresentativesPageLayout.java
@@ -1,12 +1,55 @@
 package com.mobilonix.voices.representatives.ui;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import com.mobilonix.voices.R;
+import com.mobilonix.voices.VoicesMainActivity;
+import com.mobilonix.voices.base.util.GeneralUtil;
+
 public class RepresentativesPageLayout extends LinearLayout {
+
     public RepresentativesPageLayout(Context context, AttributeSet attrs) {
+
         super(context, attrs);
+
+        hideKeyboard((VoicesMainActivity)getContext());
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        GeneralUtil.toast("Page attched!");
+
+        findViewById(R.id.google_are_assholes_layout).requestFocus();
+
+        hideKeyboard((VoicesMainActivity)getContext());
+
+        initViews();
+    }
+
+    public void initViews() {
+        //getContext().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
 }
