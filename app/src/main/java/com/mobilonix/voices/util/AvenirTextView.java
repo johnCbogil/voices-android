42d7fb08c79b2fc934ad994a6dee807ce5aa41c2
Angelica Schreyer <angelicaschreyer@Angelicas-Air.home>
2016-08-14 14:45:03 -0400

    Info icon and representatives item layout

---
+++ app/src/main/java/com/mobilonix/voices/util/AvenirTextView.java
@@ -0,0 +1,13 @@
+package com.mobilonix.voices.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class AvenirTextView extends TextView {
+    public AvenirTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/AvenirNext-Regular.ttf"));
+    }
+}
