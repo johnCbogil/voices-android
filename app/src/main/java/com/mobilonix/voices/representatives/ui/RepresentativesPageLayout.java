ee02ae38c67bf50675e29e07ef0c148aa3ad568c
lauren_quoraishee <coruscatingluminosity@gmail.com>
2017-03-23 13:03:25 -0400

    old changes

--- app/src/main/java/com/mobilonix/voices/representatives/ui/RepresentativesPageLayout.java
+++ app/src/main/java/com/mobilonix/voices/representatives/ui/RepresentativesPageLayout.java
@@ -4,20 +4,12 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.support.v7.widget.Toolbar;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.mobilonix.voices.R;
-import com.mobilonix.voices.VoicesApplication;
 import com.mobilonix.voices.VoicesMainActivity;
 import com.mobilonix.voices.representatives.model.RepresentativesPage;
 
@@ -37,52 +29,6 @@ public class RepresentativesPageLayout extends LinearLayout {
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         Toolbar toolbar = ((VoicesMainActivity)getContext()).getToolbar();
-
-        ImageView infoIcon = (ImageView)toolbar.findViewById(R.id.representatives_info_icon);
-        ImageView helpIcon = (ImageView)toolbar.findViewById(R.id.representatives_help_icon);
-
-        infoIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int voicesOrange = VoicesApplication.getContext().getResources().getColor(R.color.voices_orange);
-                responseDialog = new Dialog(getContext());
-                responseDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                responseDialog.setContentView(R.layout.dialog_info);
-                responseDialog.setTitle(VoicesApplication.getContext().getString(R.string.response_title));
-                TextView responseTextView = (TextView)responseDialog.findViewById(R.id.response);
-                String response = VoicesApplication.getContext().getString(R.string.response);
-                Spannable span = new SpannableString(response);
-                span.setSpan(new ForegroundColorSpan(voicesOrange), 17, 28, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-                span.setSpan(new ForegroundColorSpan(voicesOrange), 94, 149, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-                responseTextView.setText(span);
-                Button infoCloseButton = (Button) responseDialog.findViewById(R.id.info_close_button);
-                infoCloseButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        responseDialog.dismiss();
-                    }
-                });
-                responseDialog.show();
-            }
-        });
-
-        helpIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                helpDialog = new Dialog(getContext());
-                helpDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                helpDialog.setContentView(R.layout.dialog_instructions);
-                helpDialog.setTitle(R.string.instructions_title);
-                helpDialog.show();
-                Button gotItButton = (Button)helpDialog.findViewById(R.id.got_it_button);
-                gotItButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        helpDialog.dismiss();
-                    }
-                });
-            }
-        });
 
         findViewById(R.id.google_are_assholes_layout).requestFocus();
 
